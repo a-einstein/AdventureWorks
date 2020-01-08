3ffa202bae9d9912b6f9154181b62eea2422897d
a-einstein <aeinstein@xs4all.nl>
2020-01-08 20:13:10 +0100

    Scaffolded the datamodel (see ReadMe). Did a bit of cleanup. Not in a working order.

--- RCS.AdventureWorks.Api.Products/Program.cs
+++ RCS.AdventureWorks.Api.Products/Program.cs
@@ -1,16 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace RCS.AdventureWorks.Api.Products
 {
     public class Program
     {
+        // TODO >>> API
+        // Verder hiermee https://docs.microsoft.com/en-gb/aspnet/core/tutorials/first-web-api
+        // Creeren RCS.AdventureWorks.Products voor gemeenschappelijke acties? (edmx, private delen ProductsService.svc.cs)
+        // Hernoemen RCS.AdventureWorks.Common?
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
---
+++ RCS.AdventureWorks.Products.Standard/AwbuildVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RCS.AdventureWorks.Products.Standard
+{
+    public partial class AwbuildVersion
+    {
+        public byte SystemInformationId { get; set; }
+        public string DatabaseVersion { get; set; }
+        public DateTime VersionDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
