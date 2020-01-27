dc694e2676472d3cbffe9f5245c20d6f389eaf73
a-einstein <aeinstein@xs4all.nl>
2020-01-27 22:11:21 +0100

    Initial filtered version of GetProduct().

--- RCS.AdventureWorks.Api.Products/Controllers/ProductsController.cs
+++ RCS.AdventureWorks.Api.Products/Controllers/ProductsController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LinqKit;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using RCS.AdventureWorks.Products;
 using RCS.AdventureWorks.Products.Standard;
-using System.Collections.Generic;
+using System;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using DomainClasses = RCS.AdventureWorks.Common.DomainClasses;
 using Dtos = RCS.AdventureWorks.Common.Dtos;
@@ -23,17 +26,27 @@ namespace RCS.AdventureWorks.Api.Products.Controllers
         }
         #endregion
 
-        // Note the implicit transformation from the paths to the function names.
+        // Note the implicit transformation from the entity names to the function names.
+
         #region API
-        // GET: api/Products
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
+        [HttpGet("category={productCategoryId:int:min(1)}")]
+        [HttpGet("category={productCategoryId:int:min(1)}/subcategory={productSubcategoryId:int:min(1)}")]
+        [HttpGet("string={searchString:minlength(3)}")]
+        [HttpGet("category={productCategoryId:int:min(1)}/string={searchString:minlength(3)}")]
+        [HttpGet("category={productCategoryId:int:min(1)}/subcategory={productSubcategoryId:int:min(1)}/string={searchString:minlength(3)}")]
+        public async Task<ActionResult<Dtos.ProductsOverviewList>> GetProduct(int? productCategoryId, int? productSubcategoryId, string searchString)
         {
-            return await dbContext.Product.ToListAsync();
+            var task = Task.Run(() =>
+            {
+                var listDto = GetProductsOverview(productCategoryId, productSubcategoryId, searchString);
+
+                return listDto;
+            });
+
+            return await task.ConfigureAwait(false);
         }
 
-        // GET: api/Products/5
-        [HttpGet("{id}")]
+        [HttpGet("{id:int:min(1)}")]
         public async Task<ActionResult<DomainClasses.Product>> GetProduct(int id)
         {
             var task = Task.Run(() =>
@@ -111,9 +124,132 @@ namespace RCS.AdventureWorks.Api.Products.Controllers
         #endregion
 
         #region private
+        // Note this part is very comparable to class ProductsService, but has minor naming differences in the Entity Framework model.
+
         private bool ProductExists(int id)
         {
             return dbContext.Product.Any(e => e.ProductId == id);
+        }
+
+        private static Expression<Func<Product, bool>> CategoryTest(int? productCategoryId)
+        {
+            return product =>
+                product.ProductSubcategory != null &&
+                product.ProductSubcategory.ProductCategoryId == productCategoryId;
+        }
+
+        private static Expression<Func<Product, bool>> SubcategoryTest(int? productSubcategoryId)
+        {
+            return product =>
+                product.ProductSubcategoryId == productSubcategoryId;
+        }
+
+        private static Expression<Func<Product, bool>> StringTest(string searchString)
+        {
+            return product =>
+               product.Color.Contains(searchString) || product.Name.Contains(searchString);
+        }
+
+        private static Expression<Func<Product, bool>> ProductsFilterExpression(int? productCategoryId, int? productSubcategoryId, string searchString)
+        {
+            // Note that ProductCategory is reached through ProductSubcategory. 
+            // - Product.ProductSubcategoryId -> ProductSubcategory
+            // - ProductSubcategory.ProductCategoryId -> ProductCategory
+            // Note that Product.ProductSubcategoryID is nullable. So Product may have no ProductSubcategory and thus no ProductCategory.
+            // But for a ProductCategory to be applied on a Product, a ProductSubcategory has to be set too.
+            // This actually occurs in the current DB and has to be tested for.
+
+            var isCategoryFilter = Expressions.IsCategoryFilter(productCategoryId, productSubcategoryId, searchString);
+            var isSubcategoryFilter = Expressions.IsSubcategoryFilter(productCategoryId, productSubcategoryId, searchString);
+            var isCategoryAndStringFilter = Expressions.IsCategoryAndStringFilter(productCategoryId, productSubcategoryId, searchString);
+            var isFullFilter = Expressions.IsFullFilter(productCategoryId, productSubcategoryId, searchString);
+            var isStringFilter = Expressions.IsStringFilter(productCategoryId, productSubcategoryId, searchString);
+
+            var categoryTest = CategoryTest(productCategoryId);
+            var subcategoryTest = SubcategoryTest(productSubcategoryId);
+            var stringTest = StringTest(searchString);
+
+            // Need to Invoke on variables instead of function calls.
+            // Both arguments need to be Expressions.
+            Expression<Func<Product, bool>> categoryFilter = product =>
+                isCategoryFilter.Invoke() &&
+                categoryTest.Invoke(product);
+
+            Expression<Func<Product, bool>> subCategoryFilter = product =>
+                isSubcategoryFilter.Invoke() &&
+                subcategoryTest.Invoke(product);
+
+            Expression<Func<Product, bool>> categoryAndStringFilter = product =>
+                isCategoryAndStringFilter.Invoke() &&
+                categoryTest.Invoke(product) &&
+                stringTest.Invoke(product);
+
+            Expression<Func<Product, bool>> fullFilter = product =>
+                isFullFilter.Invoke() &&
+                categoryTest.Invoke(product) &&
+                subcategoryTest.Invoke(product) &&
+                stringTest.Invoke(product);
+
+            Expression<Func<Product, bool>> stringFilter = product =>
+                isStringFilter.Invoke() &&
+                stringTest.Invoke(product);
+
+            // The filters must be mutually exclusive.
+            return product =>
+                (fullFilter.Invoke(product) || subCategoryFilter.Invoke(product)) || categoryAndStringFilter.Invoke(product) || categoryFilter.Invoke(product) || stringFilter.Invoke(product);
+        }
+
+        private static Expression<Func<Product, DomainClasses.ProductsOverviewObject>> ProductsOverviewObjectExpression()
+        {
+            return product => new DomainClasses.ProductsOverviewObject()
+            {
+                Id = product.ProductId,
+                Name = product.Name,
+                Color = product.Color,
+                ListPrice = product.ListPrice,
+
+                Size = product.Size,
+                SizeUnitMeasureCode = product.SizeUnitMeasureCode,
+
+                WeightUnitMeasureCode = product.WeightUnitMeasureCode,
+                // Note navigation properies are still applicable.
+                ThumbNailPhoto = product.ProductProductPhoto.FirstOrDefault().ProductPhoto.ThumbNailPhoto,
+
+                ProductCategoryId = (product.ProductSubcategory != null) ? product.ProductSubcategory.ProductCategoryId : (int?)null,
+                ProductCategory = (product.ProductSubcategory != null) ? product.ProductSubcategory.ProductCategory.Name : null,
+
+                ProductSubcategoryId = (product.ProductSubcategory != null) ? product.ProductSubcategory.ProductSubcategoryId : (int?)null,
+                ProductSubcategory = (product.ProductSubcategory != null) ? product.ProductSubcategory.Name : null
+            };
+        }
+
+        private Dtos.ProductsOverviewList GetProductsOverview(int? productCategoryId, int? productSubcategoryId, string searchString)
+        {
+            // The expression is broken down using LINQKit, which extends with Invoke, Expand, AsExpandable.
+            // For details and examples:
+            // http://www.albahari.com/nutshell/linqkit.aspx
+            // https://github.com/scottksmith95/LINQKit
+            var productsFilterExpression = ProductsFilterExpression(productCategoryId, productSubcategoryId, searchString);
+            var productsOverviewObjectExpression = ProductsOverviewObjectExpression();
+
+            // Need to Expand on variables instead of function calls.
+            IQueryable<DomainClasses.ProductsOverviewObject> query =
+                dbContext.Product.AsExpandable().
+                Where(productsFilterExpression.Expand()).
+                Select(productsOverviewObjectExpression.Expand()).
+                OrderBy(product => product.Name);
+
+            var result = new Dtos.ProductsOverviewList();
+
+            // Note that the query executes on ToList.
+            var queryResult = query.ToList();
+
+            foreach (var item in queryResult)
+            {
+                result.Add(item);
+            }
+
+            return result;
         }
 
         private DomainClasses.Product GetProductDetails(int productID)
